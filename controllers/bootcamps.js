bf19fa132592ba59f7fb3dbcbb4c057a827498f6
BrendaSpencer <spencerbernadette1@gmail.com>
2023-06-17 17:26:41 +0200

    errorHandler middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,13 +15,20 @@ exports.getBootcamps = async (req, res, next) => {
 //  @route   GET /api/v1/bootcamps/:id
 //  @access  Public
 exports.getBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.findById(req.params.id);
-  if (!bootcamp) {
-    return res.status(400).json({
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+      });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({
       success: false,
+      error: error,
     });
   }
-  res.status(200).json({ success: true, data: bootcamp });
 };
 
 //  @desc    Create new bootcamp
